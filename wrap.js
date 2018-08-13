6311379d1827b1d8344b0b89acd3fd7cbdf85a48
abby wigdale <abby.wigdale@gmail.com>
2018-08-13 10:34:35 -0500

    added tests

--- wrap.js
+++ wrap.js
@@ -1,3 +1,11 @@
-const wrap = (line, maxLen) => 'I have\n a dog';
+const wrap = (line, maxLen) => {
+  if (line.length < maxLen) return line;
+  let finalString = '';
+  const firstLine = line.slice(0, maxLen);
+  const secondLine = line.slice(maxLen, maxLen + maxLen);
+  finalString = firstLine + '\n' + secondLine;
+
+  return finalString;
+};
 
 module.exports = wrap;
--- wrap.spec.js
+++ wrap.spec.js
@@ -3,9 +3,12 @@ const wrap = require('./wrap');
 
 describe('wrap', () => {
   it('Returns empty string if empty string was provided', () => {
-    expect(wrap("", 10)).to.equal("");
+    expect(wrap('', 10)).to.equal('');
   });
   it('Returns two lines if simple string is inserted', () => {
-      expect(wrap("I have a dog", 6)).to.equal("I have\n a dog");
-  }); 
+    expect(wrap('I have a dog', 6)).to.equal('I have\n a dog');
+  });
+  it('Breaks at a break point', () => {
+    expect(wrap('My name is Abby', 4)).to.equal('My\n name\n is\n Abby');
+  });
 });
